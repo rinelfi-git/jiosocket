a79f854b29832c9f9d65bd193f4d9d15ebf88138
rinelfi <elierijaniaina@gmail.com>
2021-11-18 16:33:44 +0300

    suppression de notation inutile dans TCPCallback

--- TCP/src/main/java/mg/rinelfi/jiosocket/TCPCallback.java
+++ TCP/src/main/java/mg/rinelfi/jiosocket/TCPCallback.java
@@ -1,7 +1,5 @@
 package mg.rinelfi.jiosocket;
 
-import com.sun.istack.internal.Nullable;
-
 public interface TCPCallback {
-    void update(@Nullable String data);
+    void update(String data);
 }
